f29ff0911cf23b7c8e08b6a37428247b2dd85f35
sathia27 <sathia2704@gmail.com>
2014-04-05 23:05:30 +0530

    displaying all words

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,5 +1,15 @@
 class Admin::WordsController < Admin::BaseController
   def index
-    render :json => true
+    @words = Word.all
+    respond_to do |format|
+      format.html { render action: 'index' }
+      format.json { render json: @words.to_json }
+    end
+  end
+
+  def destroy
+    @word = Word.find(params[:id])
+    @word.destroy
+    redirect_to admin_words_path
   end
 end
