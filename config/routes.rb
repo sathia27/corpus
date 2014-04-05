92ae981da21c2f555599e342f01d0a2da29741b9
sathia27 <sathia2704@gmail.com>
2014-04-05 13:40:34 +0530

    admin #8 added admin namespace in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :words
+  namespace :admin do
+    resources :words
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
