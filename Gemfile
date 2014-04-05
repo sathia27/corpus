b2bf43d53e92c4bb9ad633a35f7cf2b79791aa8b
sathia27 <sathia2704@gmail.com>
2014-04-06 00:28:33 +0530

    fixes #10 added ransack gem for filter

--- Gemfile
+++ Gemfile
@@ -12,6 +12,10 @@ group :development, :test do
 end
 gem "carrierwave"
 gem 'kaminari'
+
+gem "ransack", github: "activerecord-hackery/ransack", branch: "rails-4.1"
+gem "polyamorous", github: "activerecord-hackery/polyamorous"
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,6 +1,7 @@
 class Admin::WordsController < Admin::BaseController
   def index
-    @words = Word.page(params[:page]).per(7)
+    @q = Word.search(params[:q])
+    @words = @q.result(distinct: true).page(params[:page]).per(7)
     respond_to do |format|
       format.html { render action: 'index' }
       format.json { render json: @words.to_json }
