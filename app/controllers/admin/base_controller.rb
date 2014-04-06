390ab9309da9b0f1b1980533f6f0b3759a8861cf
sathia27 <sathia2704@gmail.com>
2014-04-06 22:06:26 +0530

    random word page css updates. main and admin layout update

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,5 @@
 class Admin::BaseController < ApplicationController
   before_filter :authenticate_admin
-  layout "admin"
   def authenticate_admin
     unless current_user.admin?
       redirect_to root_path
