4b57a4ef19d8b084ee15fe2a8c97b0463f6e11bb
sathia27 <sathia2704@gmail.com>
2014-04-16 13:20:51 +0530

    smtp errors in openshift

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -78,6 +78,16 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { host: 'http://corpus-sathia.rhcloud.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'sorkandu@gmail.com',
+    password:             'sorkandu123',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
